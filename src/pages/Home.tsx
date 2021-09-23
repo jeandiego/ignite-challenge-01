6f37f6cc6ca1c699d10e1d674bee59941fb56090
Jean Diego <jeandiego.ds@gmail.com>
2021-09-23 13:21:50 -0300

    ✨ Toggle as done added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,37 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks([...tasks, task]);
+    
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+
+   const updatedTasks = tasks.map(task => ({ ...task }))
+   const foundTask = updatedTasks.find(task => task?.id === id)
+
+   if(!foundTask) return;
+   foundTask.done = !foundTask.done;
+
+   setTasks(updatedTasks)
+}
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter((task)=> task.id !== id ))
   }
-
+  
   return (
     <View style={styles.container}>
       <Header tasksCounter={tasks.length} />
